30035fee2ed21be6dc7efbbc7d8e4078b3f5ee9a
Shubhdeep Sarkar <sarkarshubhdeep1@gmail.com>
2025-08-02 15:25:45 -0400

    Integrate framer-motion for enhanced animations in Navbar: Added motion effects for navigation links and improved responsiveness. Updated package dependencies to include motion and framer-motion.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import HamburgerMenu from "./HamburgerMenu";
 import { useState } from "react";
 import FocusButton from "./FocusButton";
+import { motion } from "motion/react";
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -20,85 +21,182 @@ export default function Navbar() {
             >
                 {/* Navigation Links */}
                 <div className="flex flex-col items-center justify-center h-full relative">
-                    <FocusButton
-                        href="#projects-section"
-                        className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
-                        onClick={() => setIsOpen(false)}
+                    <motion.div
+                        initial={{ y: 20, opacity: 0 }}
+                        animate={
+                            isOpen
+                                ? { y: 0, opacity: 1 }
+                                : { y: 40, opacity: 0 }
+                        }
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.1,
+                        }}
+                        className="w-full overflow-hidden"
                     >
-                        PROJECTS
-                    </FocusButton>
-                    <FocusButton
-                        href="#about-section"
-                        className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
-                        onClick={() => setIsOpen(false)}
+                        <FocusButton
+                            href="#projects-section"
+                            className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            PROJECTS
+                        </FocusButton>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 20, opacity: 0 }}
+                        animate={
+                            isOpen
+                                ? { y: 0, opacity: 1 }
+                                : { y: 40, opacity: 0 }
+                        }
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.2,
+                        }}
+                        className="w-full overflow-hidden"
                     >
-                        ABOUT
-                    </FocusButton>
-                    <FocusButton
-                        href="#contact-section"
-                        className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
-                        onClick={() => setIsOpen(false)}
+                        <FocusButton
+                            href="#about-section"
+                            className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            ABOUT
+                        </FocusButton>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: 20, opacity: 0 }}
+                        animate={
+                            isOpen
+                                ? { y: 0, opacity: 1 }
+                                : { y: 40, opacity: 0 }
+                        }
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.3,
+                        }}
+                        className="w-full overflow-hidden"
                     >
-                        CONTACT
-                    </FocusButton>
+                        <FocusButton
+                            href="#contact-section"
+                            className="flex items-center justify-center uppercase text-xl font-clash-grotesk-medium px-[26px] py-[16px] h-[60px] hover:bg-accent hover:text-background transition-all duration-150 w-full"
+                            onClick={() => setIsOpen(false)}
+                        >
+                            CONTACT
+                        </FocusButton>
+                    </motion.div>
                     <p className="text-sm py-[26px] text-center absolute bottom-0 w-full font-sofia-sans-regular">
                         Copyright © 2025 Shubhdeep Sarkar
                     </p>
                 </div>
             </div>
 
-            <div className="backdrop-blur-lg flex justify-between items-center bg-foreground/10 h-[80px] fixed z-20 font-clash-grotesk-medium w-full top-0 left-0 md:max-w-[800px] md:top-[120px] md:left-[80px] lg:left-[240px]">
-                <div className="group flex items-center flex-1 h-full pl-[26px] w-[160px] hover:bg-accent/80 transition-all duration-150">
+            <motion.div
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{
+                    ease: "easeIn",
+                    duration: 0.3,
+                }}
+                className="backdrop-blur-lg flex justify-between items-center bg-foreground/10 h-[80px] fixed z-20 font-clash-grotesk-medium w-full top-0 left-0 md:max-w-[800px] md:top-[120px] md:left-[80px] lg:left-[240px] overflow-hidden"
+            >
+                <motion.div
+                    initial={{ x: -40, opacity: 0 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{
+                        ease: "easeIn",
+                        duration: 0.4,
+                        delay: 0.4,
+                    }}
+                    className="group flex items-center flex-1 h-full pl-[26px] w-[160px] hover:bg-accent/80"
+                >
                     <Image
                         src="/shubhdeep-sarkar-dark.svg"
                         alt="logo"
                         width={100}
                         height={100}
-                        className="min-w-[100px] max-w-[100px] h-[100px] group-hover:hidden transition-transform"
+                        className="min-w-[100px] max-w-[100px] h-[100px] group-hover:invert"
                     />
-                    <Image
-                        src="/shubhdeep-sarkar-light.svg"
-                        alt="logo"
-                        width={100}
-                        height={100}
-                        className="min-w-[100px] max-w-[100px] h-[100px] hidden group-hover:block transition-transform"
-                    />
-                </div>
+                </motion.div>
                 <div className="hidden lg:flex items-center justify-center w-[480px] h-full">
-                    <FocusButton
-                        href="#projects-section"
-                        className="h-full flex items-center justify-center w-[120px] uppercase"
+                    <motion.div
+                        initial={{ y: -20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.4,
+                        }}
                     >
-                        PROJECTS
-                    </FocusButton>
-                    <FocusButton
-                        href="#about-section"
-                        className="h-full flex items-center justify-center w-[120px] uppercase"
+                        <FocusButton
+                            href="#projects-section"
+                            className="h-[80px] flex items-center justify-center w-[120px] uppercase"
+                        >
+                            Projects
+                        </FocusButton>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: -20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.5,
+                        }}
                     >
-                        ABOUT
-                    </FocusButton>
-                    <FocusButton
-                        href="#contact-section"
-                        className="h-full flex items-center justify-center w-[120px] uppercase"
+                        <FocusButton
+                            href="#about-section"
+                            className="h-[80px] flex items-center justify-center w-[120px] uppercase"
+                        >
+                            About
+                        </FocusButton>
+                    </motion.div>
+                    <motion.div
+                        initial={{ y: -20, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.6,
+                        }}
                     >
-                        CONTACT
-                    </FocusButton>
+                        <FocusButton
+                            href="#contact-section"
+                            className="h-[80px] flex items-center justify-center w-[120px] uppercase"
+                        >
+                            CONTACT
+                        </FocusButton>
+                    </motion.div>
                 </div>
                 {/* if the viewport is less than lg, show the hamburger menu */}
                 <div className="flex items-center justify-end flex-1 gap-2 h-full w-[160px]">
-                    <Link
-                        href="/Shubhdeep_Sarkar_Resume.pdf"
-                        className="group flex items-center justify-end gap-2 h-full pr-[26px] w-[160px] hover:bg-accent/80 hover:text-background transition-all duration-150 uppercase"
+                    <motion.div
+                        initial={{ x: 40, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{
+                            ease: "easeInOut",
+                            duration: 0.4,
+                            delay: 0.7,
+                        }}
+                        className="h-full"
                     >
-                        Resume
-                        <Image
-                            src="/arrowupright-dark.svg"
-                            alt="resume"
-                            width={18}
-                            height={20}
-                            className="group-hover:translate-y-[-20px] group-hover:translate-x-[16px] group-hover:brightness-0 hover:invert transition-all duration-150"
-                        />
-                    </Link>
+                        <Link
+                            href="/Shubhdeep_Sarkar_Resume.pdf"
+                            target="_blank"
+                            className="group  flex items-center justify-end gap-2 h-full pr-[26px] w-[160px] hover:bg-accent/80 hover:text-background transition-all duration-150 uppercase"
+                        >
+                            Resume
+                            <Image
+                                src="/arrowupright-dark.svg"
+                                alt="resume"
+                                width={18}
+                                height={20}
+                                className="group-hover:translate-y-[-20px] group-hover:translate-x-[16px] group-hover:brightness-0 hover:invert transition-all duration-150"
+                            />
+                        </Link>
+                    </motion.div>
                     <div className="lg:hidden">
                         <HamburgerMenu
                             isOpen={isOpen}
@@ -106,7 +204,7 @@ export default function Navbar() {
                         />
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </>
     );
 }
